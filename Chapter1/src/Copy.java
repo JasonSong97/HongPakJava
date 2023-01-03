ca45cd58be5348b99e70ebf85444021f6e786fb8
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-01-03 16:56:18 +0900

    Chapter1

---
+++ Chapter1/src/Copy.java
@@ -0,0 +1,9 @@
+public class Copy {
+    public static void main(String[] args){
+        String origin = "abcd";
+        String copy = origin;
+
+        copy = copy + "efgh";
+        System.out.println(copy);
+    }
+}
---
+++ Chapter1/src/Greetings.java
@@ -0,0 +1,6 @@
+public class Greetings {
+    public static void main(String[] args){
+        System.out.println("안녕 반가워!");
+        System.out.println("나는 고양이라고해");
+    }
+}
