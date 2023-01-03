0a5035b7288d015f84c8259b699ddfe8aa613d27
Jason_song97 <107292103+JasonSong97@users.noreply.github.com>
2023-01-03 21:15:59 +0900

    Chapter9

---
+++ Chapter9/src/NightChick.java
@@ -0,0 +1,19 @@
+import java.util.StringTokenizer;
+
+public class NightChick {
+    public static void main(String[] args){
+        String str1 = "야식이 생각나는 밤, 나는 그렇게 치킨을 시켰다. 그렇다.."  + "치킨은 항상 옯다.";
+        StringTokenizer st1 = new StringTokenizer(str1, ".");
+        while(st1.hasMoreTokens()){
+            System.out.println(st1.nextToken());
+        }
+
+        String str2 = "치킨,피자,족발,보쌈,etc.";
+        StringTokenizer st2 = new StringTokenizer(str2, ",");
+        while (st2.hasMoreTokens()){
+            System.out.println(st2.nextToken());
+        }
+
+    }
+}
+
